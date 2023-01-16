063bb8bc19121096c8f6aa0f715d1deeeed1ca08
truthhun <1272881215@qq.com>
2023-01-16 12:49:53 +0800

    定义_hmt

--- web/middleware/analytic.js
+++ web/middleware/analytic.js
@@ -1,6 +1,7 @@
 export default function ({ store, route, redirect }) {
   const settings = store.getters['setting/settings']
   try {
+    var _hmt = _hmt || []
     eval(settings.system.analytics)
     if (_hmt) {
       _hmt.push(['_trackPageview', route.fullPath])
